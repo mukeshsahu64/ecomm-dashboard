e0fb3f20d666398658b233b8dcf6b141c3c74b93
mukeshsahu64 <mukesh9580@gmail.com>
2022-03-22 23:21:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+     <button>Normal Button</button>
+     <Button class="btn btn-success">Bootstrap</Button>
+    </div>
+  );
+}
+
+export default App;
